f474d243efdab49d3f6b96ffd3643110a5749de0
lezasicon <lezasicon@gmail.com>
2026-03-04 11:53:15 +0200

    a

--- data.js
+++ data.js
@@ -26,6 +26,19 @@ const iconsData = [
         "status": "available",
         "description": "Exquisite icon of Saint Nicholas featuring a handcrafted wooden frame."
     },
+{
+        "id": "panagia-3",
+        "title": "Panagia Virgin Mary",
+        "price": "150€",
+        "images": [
+            "panagia-3.jpg",
+            "panagia-3-2.jpg",
+            "panagia-3-3.jpg"
+        ],
+        "ebayLink": "https://www.ebay.ie/itm/257390466789",
+        "status": "available",
+        "description": "<h3>Panagia Virgin Mary – Original Hand-Painted Greek Icon on Wood with frame</h3><p>Up for sale is a handcrafted iconography of <strong>Panagia Virgin Mary</strong>. This is a unique piece of religious art, created with devotion and traditional techniques.</p><strong>Product Features:</strong><ul><li><strong>Authenticity:</strong> 100% Hand-painted (NOT a print, NOT a lithography).</li><li><strong>Dimensions:</strong> 40x30 cm (approx. 15.75 x 11.81 inches)</li><li><strong>Material:</strong> cradled plywood wood panel.</li><li><strong>Technique:</strong> Painted by hand using a combination of acrylics and oil colors for depth and durability.</li><li><strong>Finish:</strong> Protected with a professional-grade varnish to ensure long-lasting colors and protection against moisture.</li></ul>"
+    },
     {
         "id": "agia-eleni",
         "title": "Saint Helen",
@@ -46,12 +59,12 @@ const iconsData = [
     },
     {
         "id": "iisous-christos-1",
-        "title": "Jesus Christ",
+        "title": "Jesus Christ Βridegroom",
         "price": "Sold",
         "images": ["iisous-christos-1.jpg"],
         "ebayLink": "https://www.ebay.com/itm/dummy3",
         "status": "past",
-        "description": "Byzantine icon of Jesus Christ, Pantocrator."
+        "description": "Byzantine icon of Jesus Christ, the Bridegroom."
     },
     {
         "id": "agios-nikolaos-past",
